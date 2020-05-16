2396481f9316ce046302688d1a8476b6853ff25a
MaratUmarov <pirany10@gmail.com>
2020-05-17 00:32:44 +0300

    1

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+       File Dir=new File("D:\\Dir");
+        File f = new File("D:\\Dir\\program.txt");
+        System.out.println(Dir.mkdir());
+        // Create new file
+        // if it does not exist
+        if (f.createNewFile())
+        System.out.println("File created");
+       else
+            System.out.println("File already exists");
+
+    }
+}
